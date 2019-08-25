7cad26b6ed76d4056c14b5b40947f442476a53c5
ecosse3 <theecosse@gmail.com>
2019-08-25 12:07:48 +0200

    Faked the Search with Observables

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { data } from '../mock-data'
 import { Movie } from '../Movie'
 import { MovieService } from '../movie.service'
+import { Subject } from 'rxjs'
+import { map, debounceTime } from 'rxjs/operators'
 
 
 @Component({
@@ -11,9 +13,22 @@ import { MovieService } from '../movie.service'
 })
 export class SearchComponent implements OnInit {
   searchResults:Movie[] = data
+  search$:Subject<string> = new Subject<string>()
+  fetching:boolean = false
+  search:string = ""
   constructor(private movieService:MovieService) { }
 
   ngOnInit() {
+    this.search$
+      .pipe(map(query=>{
+        this.fetching = true
+        return query
+      }), debounceTime(500))
+      .subscribe(this.searchQuery.bind(this))
+  }
+
+  searchQuery(query:string){
+    console.log(query)
   }
 
   setCurrentMovie(movie:Movie){
