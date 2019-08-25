f997bcce62ab40b17dd7d689ba5bcf4c2b4c984a
ecosse3 <theecosse@gmail.com>
2019-08-25 22:56:46 +0200

    Added type to http get param

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,67 +1,88 @@
-import { Injectable } from '@angular/core';
-import { Subject, Observable } from 'rxjs'
-import { HttpClient, HttpParams } from '@angular/common/http'
-import { Movie } from './Movie'
-import { map } from 'rxjs/operators'
+import {
+  Injectable
+} from '@angular/core';
+import {
+  Subject,
+  Observable
+} from 'rxjs'
+import {
+  HttpClient,
+  HttpParams
+} from '@angular/common/http'
+import {
+  Movie
+} from './Movie'
+import {
+  map
+} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-  private selectedMovie$:Subject<Movie> = new Subject<Movie>()
-  private apiKey:string = "2e64ee1ffe9f547ce7cf8d17286601c8"
-  private baseApiUrl:string = "https://api.themoviedb.org/3/search/movie"
-  private baseConfigurationUrl:string = "https://api.themoviedb.org/3/configuration"
-  private imageBaseUrl:string = ""
-  private imageSizes:{ backdrop?:string[], poster?:string[] } = {}
+  private selectedMovie$: Subject < Movie > = new Subject < Movie > ()
+  private apiKey: string = "2e64ee1ffe9f547ce7cf8d17286601c8"
+  private baseApiUrl: string = "https://api.themoviedb.org/3/search/movie"
+  private baseConfigurationUrl: string = "https://api.themoviedb.org/3/configuration"
+  private imageBaseUrl: string = ""
+  private imageSizes: {
+    backdrop ? : string[],
+    poster ? : string[]
+  } = {}
 
-  constructor(private http:HttpClient) {
+  constructor(private http: HttpClient) {
     this.setImageConfiguration()
   }
 
-  get currentMovie(){
+  get currentMovie() {
     return this.selectedMovie$
   }
 
-  searchMovie(query:string){
+  searchMovie(query: string) {
     const params = new HttpParams().set('api_key', this.apiKey).set('query', query)
-    return this.http.get<any>(this.baseApiUrl, { params })
-          .pipe(map(res =>
-              res.results.map((result:Movie) => {
-                return {
-                  ...result,
-                  backdropUrl:this.createPhotoUrl(result.backdrop_path, true),
-                  posterUrl:this.createPhotoUrl(result.poster_path, false)
-                }
-              })
-            )
-          )
+    return this.http.get < any > (this.baseApiUrl, {
+        params
+      })
+      .pipe(map(res =>
+        res.results.map((result: Movie) => {
+          return {
+            ...result,
+            backdropUrl: this.createPhotoUrl(result.backdrop_path, true),
+            posterUrl: this.createPhotoUrl(result.poster_path, false)
+          }
+        })
+      ))
   }
 
-  changeSelectedMovie(movie: Movie){
+  changeSelectedMovie(movie: Movie) {
     this.selectedMovie$.next(movie)
   }
 
-  setImageConfiguration(){
+  setImageConfiguration() {
     const params = new HttpParams().set('api_key', this.apiKey)
-    this.http.get(this.baseConfigurationUrl, { params })
-                .pipe(map(res => res))
-                .subscribe((config) => {
-                  this.imageBaseUrl = config.images.base_url,
-                  this.imageSizes = {
-                    backdrop:config.images.backdrop_sizes,
-                    poster:config.images.poster_sizes
-                  }
-                  console.log(this.imageBaseUrl)
-                  console.log(this.imageSizes)
-                })
+    this.http.get<any>(this.baseConfigurationUrl, {
+        params
+      })
+      .pipe(map(res => res))
+      .subscribe((config) => {
+        this.imageBaseUrl = config.images.base_url,
+          this.imageSizes = {
+            backdrop: config.images.backdrop_sizes,
+            poster: config.images.poster_sizes
+          }
+        console.log(this.imageBaseUrl)
+        console.log(this.imageSizes)
+      })
   }
-  createPhotoUrl(path:string, isBackdrop:boolean){
-    if(!path){
+  createPhotoUrl(path: string, isBackdrop: boolean) {
+    if (!path) {
       return ""
     }
-    const { backdrop, poster } = this.imageSizes
-    const imageSize = isBackdrop ? backdrop[0] : poster[ poster.length - 1 ]
+    const {
+      backdrop,
+      poster
+    } = this.imageSizes
+    const imageSize = isBackdrop ? backdrop[0] : poster[poster.length - 1]
     return `${this.imageBaseUrl}${imageSize}${path}`
   }
 
